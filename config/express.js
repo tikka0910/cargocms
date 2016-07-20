fc9fb912e8f013e1e8de20ba6aa43323842e1353
Kyle <kyle@trunk-studio.com>
2016-07-21 01:00:28 +0800

    add smart admin

---
+++ config/express.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+module.exports.express = {
+  middleware: {
+    custom: true
+  },
+  customMiddleware: function (app) {
+      console.log("######### HELLO ######");
+    // app.use(express.logger());
+    // app.use(express.compress());
+    app.use('/admin', express.static('admin/web/'));
+  }
+};
