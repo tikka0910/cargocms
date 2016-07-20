759f45d51989987a4ad132a7cf66795baf4d1350
smlsunxie <smlsun@gmail.com>
2016-07-20 23:50:44 +0800

    fix build

--- tasks/register/build.js
+++ tasks/register/build.js
@@ -1,8 +1,10 @@
 module.exports = function (grunt) {
 	grunt.registerTask('build', [
+		'bower:dev',
 		'compileAssets',
 		'linkAssetsBuild',
 		// 'clean:build',
+		'sass:dev',
 		'copy:build'
 	]);
 };
