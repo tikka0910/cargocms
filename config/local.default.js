9d12c12296517717cb492a37a92635a0cbbfefc6
smlsunxie <smlsun@gmail.com>
2016-07-25 22:59:07 +0800

    remove token

--- config/local.default.js
+++ config/local.default.js
@@ -8,8 +8,8 @@ module.exports = {
       protocol: 'oauth2',
       strategy: require('passport-facebook').Strategy,
       options: {
-        clientID: '144219216008720',
-        clientSecret: '3a76f7e46520643c445e6360ecc9e221',
+        clientID: '',
+        clientSecret: '',
         callbackURL: "http://localhost:5001/auth/facebook/callback",
         profileFields: [
           'id', 'email', 'gender', 'link', 'locale',
