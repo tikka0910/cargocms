562459239f75338e5a6ebfdb8b22e913306609a1
Kuyen <iamcxa@gmail.com>
2016-07-27 13:50:15 +0800

    update spec

--- test/unit/controllers/UserController.spec.js
+++ test/unit/controllers/UserController.spec.js
@@ -4,7 +4,6 @@ describe.skip('about User Controller operation.', function() {
   });
 
   describe('find user', () => {
-
     it('should success.', async (done) => {
       try {
         let res = await request(sails.hooks.http.app).get(`/user/find`);
@@ -15,26 +14,65 @@ describe.skip('about User Controller operation.', function() {
         done(e);
       }
     });
-
   });
 
   describe('delete user', () => {
     before(async (done) => {
-      done();
+      try {
+        const testUser = UserService.create({
+          username: 'xxxx',
+          email: 'xxx@xxx.xxx',
+          age: 25,
+        });
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
 
     it('should success.', async (done) => {
-      done(new Error('no implement'));
+      try {
+        const res = await request(sails.hooks.http.app)
+                        .delete(`/user/${testUser.id}`);
+        const { result } = res.body;
+        result.should.be.equal(true);
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
   });
 
   describe('update user', () => {
     before(async (done) => {
-      done();
+      try {
+        const createdUsed = UserService.crate({
+          username: 'xxxx',
+          email: 'xxx@xxx.xxx',
+          age: 25,
+        });
+        const updatedUser = {
+          username: 'update',
+          email: 'update@update.update',
+          age: 999,
+        };
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
 
     it('should success.', async (done) => {
-      done(new Error('no implement'));
+      try {
+        const res = await request(sails.hooks.http.app)
+                          .put(`/user/${createdUsed.id}`);
+        const { user } = res.body;
+        user.age.should.be.equal(999);
+        user.username.should.be.equal('update');
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
   });
 
