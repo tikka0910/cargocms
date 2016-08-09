c1774bf0b2cd92b090c7b6c131fa9f8867186cf6
smlsunxie <smlsun@gmail.com>
2016-08-09 23:13:29 +0800

    remove blueprints

--- config/routes.js
+++ config/routes.js
@@ -31,9 +31,7 @@ module.exports.routes = {
   * `assets` directory)                                                      *
   *                                                                          *
   ***************************************************************************/
-  '/admin/mock': "admin/MockController.index",
-  '/api/mock': "admin/MockController.find",
-
+  '/api/admin/mock': "admin/MockController.find",
   '/api/labfnp/scent': 'labfnp/ScentController.index',
 
   '/': {
@@ -60,7 +58,29 @@ module.exports.routes = {
   'get /auth/:provider/callback': 'AuthController.callback',
   'get /auth/:provider/:action': 'AuthController.callback',
 
+  //----- UserController -----
+  'get /api/user': 'UserController.find',
+  'get /api/user/:id': 'UserController.findOne',
+
+  'post /api/user': 'UserController.create',
+  'put /api/user/:id': 'UserController.update',
+  'delete /api/user/:id': 'UserController.destroy',
+
+
+  //----- PostController -----
+  'get /api/post': 'PostController.find',
+  'get /api/post/:id': 'PostController.findOne',
+
+  'post /api/post': 'PostController.create',
+  'put /api/post/:id': 'PostController.update',
+  'delete /api/post/:id': 'PostController.destroy',
+
   '/admin/config.js': "AdminController.config",
+
+  "/api/:controller/:action/:id?": {},
+  "/labfnp/:controller/:action/:id?": {},
+  "/admin/:controller/:action/:id?": {},
+  "/:controller/:action/:id?": {},
 
 
   /***************************************************************************
