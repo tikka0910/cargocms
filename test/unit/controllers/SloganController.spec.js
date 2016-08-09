b2022d0b21a8824a406982e6046ddf099c58ead7
KenYA <tikka0910@gmail.com>
2016-08-09 14:39:14 +0800

    slogan controller spec test.

---
+++ test/unit/controllers/SloganController.spec.js
@@ -0,0 +1,31 @@
+describe('About Slogan Controller operation',function(){
+  describe('Get one Slogan',function(){
+    let test_slogan = null;
+    before(async (done) => {
+      try{
+        test_slogan = await Slogan.create({
+          content:'Just Do it.',
+          source:'NIKE, Inc.'
+        });
+        done();
+      }
+      catch(e){
+        done(e);
+      }
+    });
+
+    it('get one Slogan should be success', async (done) => {
+      try{
+        const result = await request(sails.hooks.http.app)
+        .get(`/slogan/${test_slogan.id}`);
+
+        result.content.should.be.eq(test_slogan.content);
+        result.source.should.be.eq(test_slogan.source);
+        done();
+      }
+      catch(e){
+        done(e);
+      }
+    });
+  });
+});
