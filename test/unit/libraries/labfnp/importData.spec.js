4e8a20c936c46c3d692290dcb9c3addd9403d51d
smlsunxie <smlsun@gmail.com>
2016-08-09 17:47:46 +0800

    update

--- test/unit/libraries/labfnp/importData.spec.js
+++ test/unit/libraries/labfnp/importData.spec.js
@@ -1,9 +1,60 @@
-describe('import data.', () => {
+var feelingData = require("./feeling.json");
 
-  it('import feeling', (done) => {
+describe.only('import data.', () => {
+  /*
+  {
+    "Scent":"BU2",
+    "cfeeling":"木質",
+    "totalrepeat":18,
+    "score":6,
+    "color":"#B35721"
+  },
+  */
+
+
+  it('import feeling', async (done) => {
+    console.log(feelingData.length);
+
+    try {
+      for (let feelingRow of feelingData) {
+
+        let newScentNote = {
+            color: feelingRow.color,
+            title: feelingRow.color
+        }
+
+        let newScent = {
+          name: feelingRow.Scent
+        }
+        let newFeeling = {
+            title: feelingRow.cfeeling
+        }
+
+        let scentNote = (await ScentNote.findOrCreate({
+          where: {color: newScentNote.color},
+          defaults: newScentNote
+        }))[0];
+
+
+        let scent = (await Scent.findOrCreate({
+          where: {name: newScent.name},
+          defaults: newScent
+        }))[0];
+
+        let feeling = (await Feeling.findOrCreate({
+          where: {title: newFeeling.title},
+          defaults: newFeeling
+        }))[0];
+
+        await scentNote.addScent(scent)
+        await scent.addFeeling(feeling);
+      }
+      done();
+
+    } catch (e) {
+      done(e);
+    }
 
   });
-
-
 
 });
