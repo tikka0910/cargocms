980f8c4eeb6aaaa959e86672a43d53d9c0cc74bf
FuYaoDe <dan82625@gmail.com>
2016-08-09 15:17:08 +0800

    add post index

--- api/controllers/BlogController.js
+++ api/controllers/BlogController.js
@@ -1,8 +1,12 @@
 module.exports = {
   index: async (req, res) => {
     try {
+
       res.view('blog/index', {
-        
+        message: 'blog index',
+        data: {
+          items: await Post.findAll({ include: Tag }),
+        }
       });
     } catch (e) {
       res.serverError(e);
