a36222e17dccdc9346986e3d97b78a220c9a29d2
Kuyen <iamcxa@gmail.com>
2016-08-11 09:56:05 +0800

    add pre-set  data for recipe

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -59,8 +59,6 @@ module.exports.bootstrap = async (cb) => {
     });
     let passport = await Passport.create({provider: 'local', password: 'user', UserId: user.id});
 
-
-
     let admin = {
       username: 'admin',
       email: 'admin@gmail.com',
@@ -99,7 +97,7 @@ module.exports.bootstrap = async (cb) => {
         type: 'image/jpeg',
         storage: 'url',
       });
-      
+
       const post = await Post.create({
         title: '香味的一沙一世界5',
         content: '<p>我們可以這樣形容，當你手中捧到一束花時，可以聞到花束中的各種花材（ex:玫瑰、康乃馨..等)所組成的『這束花的味道』，接著抽出其中的一朵康乃馨，聞到的則是這『一朵康乃馨的味道』，而事實上，若深入去探究此康乃馨的氣味組成，則可小到香味分子的程度：花香由多種香味分子所組成。而這樣的組成就像香水、香料以及香味分子的不同級距來看，香水聞起來是一種味道而細究卻是多種香味分子的組成。</p><p>或許你會以為我們跟台北火車站、天水街的化工行在做一樣的，但實際上，於過去台灣可以找到的香味素材，大多皆已被調好的香精，而這些香精的比例對於供應商來說時常被視為商業機密，所以當你選用了某一供應商所調製的香精時很難了解到其內容組成，而為了調出一樣的味道，你只能持續的跟他買香精、香料。這樣的環境下，對於調香創作的自由度與香味分子本質掌握、認識的程度就會變得非常的低，而LFP秉持著期望每個創作者都可以實際了解香味分子的感受，才能從本質上自主選擇。</p>',
@@ -119,6 +117,20 @@ module.exports.bootstrap = async (cb) => {
 
       // let path = "";
       // await ScentNote.importFeelingFromFile({path});
+
+      const recipe = {
+        formula:[
+          {"drops":"1","scent":"M6"},
+          {"drops":"2","scent":"M10"}
+        ],
+        formulaLogs: '',
+        authorName: '王大明',
+        perfumeName: 'love',
+        message: 'this is love',
+        UserId: 1,
+      };
+
+      await RecipeService.create(recipe);
 
 
 
