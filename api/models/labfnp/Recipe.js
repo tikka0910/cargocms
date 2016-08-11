c235a9690a7383452e22f79897f0869cd3e2543c
Kuyen <iamcxa@gmail.com>
2016-08-11 10:23:04 +0800

    add message field

--- api/models/labfnp/Recipe.js
+++ api/models/labfnp/Recipe.js
@@ -19,6 +19,9 @@ module.exports = {
     },
     perfumeName: {
       type: Sequelize.STRING,
+    },
+    message: {
+      type: Sequelize.STRING,
     }
   },
   associations: function() {
@@ -35,15 +38,6 @@ module.exports = {
           },
           include: User,
         });
-        console.log("displayName=>", recipes.User.displayName);
-        // if (recipes) {
-        //   recipeWithScent = recipes.toJSON();
-        //   const scents = await Scent.findAll();
-        //   const scentArray = scents.map((scent) => scent.name);
-        //   recipeWithScent.scents = scentArray;
-        //
-        //   console.log("recipeWithScent=>", recipeWithScent);
-        // }
         return recipes;
       },
       deleteById: async (id) => {
