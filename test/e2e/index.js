dc9d5391c35a55f6e5fb6bd385c93aad26cd51f0
smlsunxie <smlsun@gmail.com>
2016-08-17 10:06:48 +0800

    add login e2e

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -5,6 +5,23 @@ describe('main page test', () => {
     it('should be get broser title of LFP @watch', () => {
       browser.url('http://localhost:1338/');
       expect(browser.getTitle()).to.equal('LFP: 香料香水實驗室，客製專屬香水');
+
+
+    });
+    it('login as admin @watch', () => {
+      browser.click('#login');
+      browser.setValue('#identifier', 'admin')
+      browser.setValue('#password', 'admin')
+      browser.click('#submit-button');
+      expect(browser.getTitle()).to.equal('LFP: 香料香水實驗室，客製專屬香水');
+
+      expect(browser.elements('#logout-link')!=null).to.equal(true);
+
+    });
+
+    it('logout @watch', () => {
+      browser.click('#logout-link');
+      expect(browser.elements('#login')!=null).to.equal(true);
     });
   });
 });
