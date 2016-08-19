927d403d6e66ea915b1827ecf20f4abedb2d6f13
smlsunxie <smlsun@gmail.com>
2016-08-19 11:47:20 +0800

    update

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,28 +1,10 @@
 require("../bootstrap.test.js")
-describe('main page test', () => {
-	describe('get index title', () => {
-		it('should be get broser title of LFP @watch', () => {
-      var ip = require("ip");
-      console.dir ( ip.address() );
+import {login} from "../bootstrap.e2e.js"
+login("admin");
 
-			browser.windowHandleSize({width:1280,height:900}).url('/');
-
-			expect(browser.getTitle()).to.equal('LFP: 香料香水實驗室，客製專屬香水');
-		});
-		it('login as admin @watch', () => {
-			browser.click('#login');
-			browser.setValue('#identifier', 'admin')
-			browser.setValue('#password', 'admin')
-			browser.click('#submit-button');
-
-			expect(browser.getTitle()).to.equal('LFP: 香料香水實驗室，客製專屬香水');
-			browser.element('#logout-link').state.should.be.equal('success');
-		});
-
-		it('logout @watch', () => {
-			browser.click('#logout-link');
-
-			browser.element('#logout-link').state.should.be.equal('failure');
-		});
+describe('test logout', () => {
+	it('logout @watch', () => {
+		browser.click('#logout-link');
+		browser.element('#logout-link').state.should.be.equal('failure');
 	});
 });
