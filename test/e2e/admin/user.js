28374a156cfe4c63100137daf491b7af5bd43cae
jimmy8319045 <jimmy8319045@gmail.com>
2016-08-22 14:18:28 +0800

    修改pause為waitFor

--- test/e2e/admin/user.js
+++ test/e2e/admin/user.js
@@ -1,4 +1,3 @@
-
 require("../../bootstrap.test.js")
 describe.only('main login page test', () => {
   describe('get index title', () => {
@@ -21,43 +20,37 @@ describe.only('main login page test', () => {
     it('test user create @watch', async (done) => {
       try {
         browser.url('http://localhost:1338/admin/#/admin/user');
-        await browser.pause(500);
-        browser.click('[id="ToolTables_main-table_1"]');
-        await browser.pause(1000);//must
+        await browser.waitForExist('#ToolTables_main-table_1',1000);
+        browser.click('#ToolTables_main-table_1');
+        await browser.waitForExist('[class="btn btn-primary"]',1000);
         browser.setValue('[name="username"]', 'usertest1');
-        await browser.pause(100);
+        await browser.waitForValue('[name="username"]',500);
         browser.setValue('[name="email"]', 'usertest1@gmail.com');
-        await browser.pause(100);
+        await browser.waitForValue('[name="email"]',500);
         browser.setValue('[name="firstName"]', '王');
-        await browser.pause(100);
+        await browser.waitForValue('[name="firstName"]',500);
         browser.setValue('[name="lastName"]', '雇員');
-        await browser.pause(100);
+        await browser.waitForValue('[name="lastName"]',500);
         browser.setValue('[name="password"]', '0000');
-        await browser.pause(100);
+        await browser.waitForValue('[name="password"]',500);
         browser.setValue('[name="passwordConfirm"]', '0000');
-        await browser.pause(100);//must
-        browser.click('[class="btn btn-primary"]')
-
-        expect(browser.elements('["class="btn btn-primary"]')!=null).to.equal(true);
-        // expect(browser.getTitle()).to.equal('會員資料');
-
+        await browser.waitForValue('[name="passwordConfirm"]',500);
+        browser.click('[class="btn btn-primary"]');
+        await browser.waitForExist('[class="btn btn-primary"]',1000,true);
+        // console.log(`!!!${browser.getText('[class="dataTables_info"]').text}`);
+        expect(browser.elements('#ToolTables_main-table_1')!=null).to.equal(true);
         done();
       } catch (e) {
         done(e);
       }
     });
 
-    it('logout @watch', async (done) => {
+    it.skip('logout @watch', async (done) => {
       try {
-        await browser.pause(1000);//must
+        //await browser.pause(1000);//must
         browser.url('http://localhost:1338/logout?url=/admin/login');
-        // browser.click('[title="Sign Out"]');
-        // await browser.pause(500);
-        // browser.click('#bot2-Msg1');
-        // await browser.pause(500);
-        // console.log(`!!!${browser.getTitle()}`);
         expect(browser.elements('[class="btn-u btn-u-blue btn-block rounded"]')!=null).to.equal(true);
-        //await browser.pause(20000);
+        await browser.pause(2000);
         done();
       } catch (e) {
         done(e);
