3e626b8351727a2f3143398b61f496ef5e2380bf
jimmy8319045 <jimmy8319045@gmail.com>
2016-08-22 15:07:45 +0800

    update

--- test/e2e/admin/user.js
+++ test/e2e/admin/user.js
@@ -5,7 +5,7 @@ describe.only('main login page test', () => {
       browser.url('http://localhost:1338/admin/login');
       expect(browser.getTitle()).to.equal('Login 2 | Unify - Responsive Website Template');
     });
-    it('login as admin @watch', async (done) => {
+    it('login as admin @watch', (done) => {
       try {
         browser.setValue('[name="identifier"]', 'admin');
         browser.setValue('[name="password"]', 'admin');
@@ -17,40 +17,50 @@ describe.only('main login page test', () => {
       }
     });
 
-    it('test user create @watch', async (done) => {
+    it('test user create @watch', (done) => {
       try {
+        const userData = {
+          name: 'usertest1',
+          email: 'usertest1@gmail.com',
+          firstName: '王',
+          lastName: '雇員',
+          password: '0000'
+        };
         browser.url('http://localhost:1338/admin/#/admin/user');
-        await browser.waitForExist('#ToolTables_main-table_1',1000);
+        browser.waitForExist('#ToolTables_main-table_1',1000);
         browser.click('#ToolTables_main-table_1');
-        await browser.waitForExist('[class="btn btn-primary"]',1000);
-        browser.setValue('[name="username"]', 'usertest1');
-        await browser.waitForValue('[name="username"]',500);
-        browser.setValue('[name="email"]', 'usertest1@gmail.com');
-        await browser.waitForValue('[name="email"]',500);
-        browser.setValue('[name="firstName"]', '王');
-        await browser.waitForValue('[name="firstName"]',500);
-        browser.setValue('[name="lastName"]', '雇員');
-        await browser.waitForValue('[name="lastName"]',500);
-        browser.setValue('[name="password"]', '0000');
-        await browser.waitForValue('[name="password"]',500);
-        browser.setValue('[name="passwordConfirm"]', '0000');
-        await browser.waitForValue('[name="passwordConfirm"]',500);
+        browser.waitForExist('[class="btn btn-primary"]',1000);
+        browser.setValue('[name="username"]', userData.name);
+        // await browser.waitForValue('[name="username"]',500);
+        browser.setValue('[name="email"]', userData.email);
+        // await browser.waitForValue('[name="email"]',500);
+        browser.setValue('[name="firstName"]', userData.firstName);
+        // await browser.waitForValue('[name="firstName"]',500);
+        browser.setValue('[name="lastName"]', userData.lastName);
+        // await browser.waitForValue('[name="lastName"]',500);
+        browser.setValue('[name="password"]', userData.password);
+        // await browser.waitForValue('[name="password"]',500);
+        browser.setValue('[name="passwordConfirm"]', userData.password);
+        // await browser.waitForValue('[name="passwordConfirm"]',500);
         browser.click('[class="btn btn-primary"]');
-        await browser.waitForExist('[class="btn btn-primary"]',1000,true);
-        // console.log(`!!!${browser.getText('[class="dataTables_info"]').text}`);
-        expect(browser.elements('#ToolTables_main-table_1')!=null).to.equal(true);
+        browser.waitForExist('[class="btn btn-primary"]',1000,true);
+
+        browser.setValue('[type=search]', userData.name);
+        browser.getText('#main-table > tbody > tr > td.expand > a').should.be.equal( userData.name );
+
+        // expect(browser.elements('#ToolTables_main-table_1')!=null).to.equal(true);
         done();
       } catch (e) {
         done(e);
       }
     });
 
-    it.skip('logout @watch', async (done) => {
+    it('logout @watch', (done) => {
       try {
         //await browser.pause(1000);//must
         browser.url('http://localhost:1338/logout?url=/admin/login');
         expect(browser.elements('[class="btn-u btn-u-blue btn-block rounded"]')!=null).to.equal(true);
-        await browser.pause(2000);
+        browser.pause(2000);
         done();
       } catch (e) {
         done(e);
