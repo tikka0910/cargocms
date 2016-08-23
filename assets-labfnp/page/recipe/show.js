b3ff24e6c6b6d2f9a180a6bb14b5383eff960dfa
FuYaoDe <dan82625@gmail.com>
2016-08-23 15:15:27 +0800

    d3 cloud

--- assets-labfnp/page/recipe/show.js
+++ assets-labfnp/page/recipe/show.js
@@ -117,3 +117,39 @@ $("#likeButton").on("click", function (event) {
 		$.get('/api/labfnp/recipe/like/' + id).done(successCatch).fail(failCatch);
 	}
 })
+
+
+var fill = d3.scale.category20();
+
+var layout = d3.layout.cloud()
+    .size([230, 230])
+    .words(["木質","日曬干草","紙味","沉靜","和平","日式房子","小木屋","大樹","清爽的樹皮","老舊的","風吹過木頭","走在阿公家","男人味","帥的人味","溫柔男子","中年客人的味道","CK味","噴太多古龍水","外國人","暖甜","柔和","厚實","深沈","辛辣","酒吧","蒸餾酒","瑪格麗特（調酒）","Vodka","琥珀","古龍水","森林","芬多精","麥克筆的味道","顏料","膨膨沐浴露","木質香","水果香","濃郁","甜","花蜜","繡球花","清新","薄荷糖","深沈","酸","酸豆","叛逆","麥克筆","指甲油","香蕉油"]
+    .map(function(d) {
+      return {text: d, size: 2 + Math.random() * 20, test: "haha"};
+    }))
+    .padding(5)
+    .rotate(function() { return 0; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw);
+
+layout.start();
+
+function draw(words) {
+  d3.select("#cloud").append("svg")
+      .attr("width", layout.size()[0])
+      .attr("height", layout.size()[1])
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+}
