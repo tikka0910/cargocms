bd146872eca66d1665dc461c9ed0635c8beb0543
smlsunxie <smlsun@gmail.com>
2016-08-27 17:36:11 +0800

    新增 post social service

--- api/services/SocialService.js
+++ api/services/SocialService.js
@@ -22,5 +22,29 @@ module.exports = {
     } catch (e) {
       throw e;
     }
+  },
+
+  forPost: ({posts}) => {
+    try {
+      let socialsConfig = sails.config.socials
+
+      const socialData = posts.map((post) => {
+        const {id, title} = post;
+        const description = "";
+        const url = sails.getBaseUrl() + '/blog/show/' + id
+        return {
+          description, title, url
+        }
+      });
+
+      let social = {
+        data: socialData,
+        targets: socialsConfig
+      };
+      return social;
+    } catch (e) {
+      throw e;
+    }
   }
+
 }
