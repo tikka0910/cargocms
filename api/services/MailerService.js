cb8ec8468bb55297ceb975e9aac355fb24e159c7
smlsunxie <smlsun@gmail.com>
2016-08-30 15:34:29 +0800

    remove log

--- api/services/MailerService.js
+++ api/services/MailerService.js
@@ -1,3 +1,2 @@
 import MailerService from 'sails-service-mailer';
-console.log("=== MailerService ===", sails.config.mail.type);
 module.exports = MailerService(sails.config.mail.type, sails.config.mail.config);
