8309e0f3fb04ca0c4359d471bd06233195646c78
smlsunxie <smlsun@gmail.com>
2016-08-30 17:56:11 +0800

    controller add response params for follow

--- api/controllers/labfnp/MainController.js
+++ api/controllers/labfnp/MainController.js
@@ -35,15 +35,19 @@ module.exports = {
       if(!user)
         return res.redirect("/login");
     }
+    const recipes = await Recipe.findAll({
+      where: { userId: user.id },
+      order: 'Recipe.updatedAt desc',
+      include: Image,
+    })
+
+    let followers = 0
+    let starred = 0
+    let Following = 0
 
     try {
       return res.view({
-        user,
-        recipes: await Recipe.findAll({
-          where: { userId: user.id },
-          order: 'Recipe.updatedAt desc',
-          include: Image,
-        })
+        user, recipes, followers, starred, following
       });
     }
     catch (e) {
