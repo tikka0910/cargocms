34a61f70bb194a5d11eb1e31afe48be92dd910fb
KenYA <tikka0910@gmail.com>
2016-08-30 09:51:40 +0800

    add find

--- api/controllers/api/AllpayController.js
+++ api/controllers/api/AllpayController.js
@@ -1,4 +1,22 @@
 module.exports = {
+  
+  find: async (req, res) => {
+    try {
+      const { query } = req;
+      const { serverSidePaging } = query;
+      const modelName = req.options.controller.split("/").reverse()[0];
+      let result;
+      if (serverSidePaging) {
+        result = await PagingService.process({query, modelName});
+      } else {
+        const items = await sails.models[modelName].findAll();
+        result = { data: { items } };
+      }
+      res.ok(result);
+    } catch (e) {
+      res.serverError(e);
+    }
+  },
 
   paid: async (req, res) => {
     try {
