f3a41b3ddf40c03832c324d7cc0478a0ba95a461
KenYA <tikka0910@gmail.com>
2016-08-31 15:22:22 +0800

    - findOne include RecipeOrder 資料 - update 將 Allpay 與 RecipeOrder 資料拆分，使用個自的更新方法 - destroy 刪除 Allpay 資料連帶刪除 RecipeOrder 資料

--- api/controllers/api/AllpayController.js
+++ api/controllers/api/AllpayController.js
@@ -9,7 +9,12 @@ module.exports = {
       if (serverSidePaging) {
         result = await PagingService.process({query, modelName});
       } else {
-        const items = await sails.models[modelName].findAll();
+        const items = await sails.models[modelName].findAll({
+          include:{
+            model: RecipeOrder,
+            include: [User, Recipe]
+          }
+        });
         result = { data: { items } };
       }
       res.ok(result);
@@ -21,7 +26,15 @@ module.exports = {
   findOne: async (req, res) => {
     try {
       const { id } = req.params;
-      const item = await Allpay.findOne({ id });
+      const item = await Allpay.findOne({
+        where:{
+          id
+        },
+        include:{
+          model: RecipeOrder,
+          include: [User, Recipe]
+        }
+       });
       res.ok({ data: { item } });
     } catch (e) {
       res.serverError(e);
@@ -33,10 +46,69 @@ module.exports = {
       const { id } = req.params;
       const data = req.body;
       const message = 'Update success.';
-      const item = await Allpay.update(data ,{
+      //allpay , recipeOrder 分拆更新資料
+      const allpayData = {
+        TradeNo: data.TradeNo,
+        MerchantTradeNo: data.MerchantTradeNo,
+        RtnCode: data.RtnCode,
+        RtnMsg: data.RtnMsg,
+        PaymentDate: data.PaymentDate,
+        TradeDate: data.TradeDate,
+        PaymentType: data.PaymentType,
+        ShouldTradeAmt: data.ShouldTradeAmt,
+        TradeAmt: data.TradeAmt,
+        BankCode: data.BankCode,
+        vAccount: data.vAccount,
+        ExpireDate: data.ExpireDate,
+        PaymentNo: data.PaymentNo,
+        Barcode1: data.Barcode1,
+        Barcode2: data.Barcode2,
+        Barcode3: data.Barcode3,
+        CheckMacValue: data.CheckMacValue,
+        MerchantTradeDate: data.MerchantTradeDate,
+        RecipeOrderId: data.RecipeOrderId,
+      };
+      const recipeData = {
+        recipient: data.recipient,
+        address: data.Address,
+        phone: data.Phone,
+        email: data.Email,
+        note: data.Note,
+        remark: data.Remark,
+      };
+
+      const allpay = await Allpay.update(allpayData ,{
         where: { id, },
       });
-      res.ok({ message, data: { item } });
+
+      const order = await RecipeOrder.update(recipeData, {
+        where: { id: data.RecipeOrderId, },
+      });
+
+      res.ok({ message, data: { allpay, order} });
+    } catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  destroy: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const recipeOrder = await Allpay.findOne({
+        where:{
+          id
+        },
+        include:{
+          model: RecipeOrder,
+          include: [User, Recipe]
+        }
+       });
+
+      const allpay = await Allpay.deleteById(id);
+      const order = await RecipeOrder.deleteById(recipeOrder.RecipeOrderId);
+
+      const message = 'Delete success.';
+      res.ok({ message, data: { allpay, order } });
     } catch (e) {
       res.serverError(e);
     }
