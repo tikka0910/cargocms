f7da866106c6bfc072701f85069170018439e220
KenYA <tikka0910@gmail.com>
2016-08-31 16:58:17 +0800

    Allpay 資料不能更動，改為只更新 RecipeOrder 的資料

--- api/controllers/api/AllpayController.js
+++ api/controllers/api/AllpayController.js
@@ -47,27 +47,27 @@ module.exports = {
       const data = req.body;
       const message = 'Update success.';
       //allpay , recipeOrder 分拆更新資料
-      const allpayData = {
-        TradeNo: data.TradeNo,
-        MerchantTradeNo: data.MerchantTradeNo,
-        RtnCode: data.RtnCode,
-        RtnMsg: data.RtnMsg,
-        PaymentDate: data.PaymentDate,
-        TradeDate: data.TradeDate,
-        PaymentType: data.PaymentType,
-        ShouldTradeAmt: data.ShouldTradeAmt,
-        TradeAmt: data.TradeAmt,
-        BankCode: data.BankCode,
-        vAccount: data.vAccount,
-        ExpireDate: data.ExpireDate,
-        PaymentNo: data.PaymentNo,
-        Barcode1: data.Barcode1,
-        Barcode2: data.Barcode2,
-        Barcode3: data.Barcode3,
-        CheckMacValue: data.CheckMacValue,
-        MerchantTradeDate: data.MerchantTradeDate,
-        RecipeOrderId: data.RecipeOrderId,
-      };
+      // const allpayData = {
+      //   TradeNo: data.TradeNo,
+      //   MerchantTradeNo: data.MerchantTradeNo,
+      //   RtnCode: data.RtnCode,
+      //   RtnMsg: data.RtnMsg,
+      //   PaymentDate: data.PaymentDate,
+      //   TradeDate: data.TradeDate,
+      //   PaymentType: data.PaymentType,
+      //   ShouldTradeAmt: data.ShouldTradeAmt,
+      //   TradeAmt: data.TradeAmt,
+      //   BankCode: data.BankCode,
+      //   vAccount: data.vAccount,
+      //   ExpireDate: data.ExpireDate,
+      //   PaymentNo: data.PaymentNo,
+      //   Barcode1: data.Barcode1,
+      //   Barcode2: data.Barcode2,
+      //   Barcode3: data.Barcode3,
+      //   CheckMacValue: data.CheckMacValue,
+      //   MerchantTradeDate: data.MerchantTradeDate,
+      //   RecipeOrderId: data.RecipeOrderId,
+      // };
       const recipeData = {
         recipient: data.recipient,
         address: data.Address,
@@ -77,15 +77,15 @@ module.exports = {
         remark: data.Remark,
       };
 
-      const allpay = await Allpay.update(allpayData ,{
-        where: { id, },
-      });
+      // const allpay = await Allpay.update(allpayData ,{
+      //   where: { id, },
+      // });
 
       const order = await RecipeOrder.update(recipeData, {
         where: { id: data.RecipeOrderId, },
       });
 
-      res.ok({ message, data: { allpay, order} });
+      res.ok({ message, data: { order } });
     } catch (e) {
       res.serverError(e);
     }
