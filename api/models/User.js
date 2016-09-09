a008b5551d4495132617107b8539ca43ec545e22
smlsunxie <smlsun@gmail.com>
2016-09-09 15:00:36 +0800

    fix user model

--- api/models/User.js
+++ api/models/User.js
@@ -74,9 +74,6 @@ module.exports = {
       type: Sequelize.INTEGER,
       defaultValue: 0
     },
-    legacyId: {
-      type: Sequelize.STRING
-    },
     updatedAt: {
       type: Sequelize.DATE,
       get: function() {
