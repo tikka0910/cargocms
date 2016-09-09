e9fd0a9ab121cd5e7812cab51e2f1d62d9845ce5
Kuyen <iamcxa@gmail.com>
2016-09-09 19:40:59 +0800

    update og image tag

--- api/controllers/labfnp/RecipeController.js
+++ api/controllers/labfnp/RecipeController.js
@@ -36,6 +36,7 @@ module.exports = {
 
   show: async function(req, res) {
     const { id } = req.params;
+    sails.log.info('res=>',res);
     try {
       const currentUser = AuthService.getSessionUser(req);
       // if (!currentUser) return res.redirect('/login');
