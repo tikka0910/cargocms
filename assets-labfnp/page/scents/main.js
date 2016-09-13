e6a15b1aa69f8e04040d454978dd5effd26591fe
Kuyen <iamcxa@gmail.com>
2016-09-13 18:22:49 +0800

    fix edit recipe by feeling

--- assets-labfnp/page/scents/main.js
+++ assets-labfnp/page/scents/main.js
@@ -205,7 +205,7 @@ $(function() {
         scentDetail.find(".tags").append('<div class="tag">' + e + '</div>')
       });
       var Text = feelingScentsCategories.val();
-      console.log(feelingScentsCategories[0]);
+      // console.log(feelingScentsCategories[0]);
       if(tags.indexOf(Text) === -1){
         scentDetail.find(".tags").append('<div class="tag">' + Text + '</div>')
       }
