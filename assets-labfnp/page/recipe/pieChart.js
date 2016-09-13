c0fa2ea6fe710d2aa653092c3e34a91ebf60c30e
Kuyen <iamcxa@gmail.com>
2016-09-13 16:36:26 +0800

    upadte pie chart

--- assets-labfnp/page/recipe/pieChart.js
+++ assets-labfnp/page/recipe/pieChart.js
@@ -21,66 +21,121 @@ var pieFooter = {
   "location": "bottom-left"
 };
 var pieSize = {
-  "canvasWidth": 230,
-  "canvasHeight": 230,
+  "canvasWidth": 400,
+  "canvasHeight": 400,
   "pieOuterRadius": "75%"
 };
-var pieDate = {
+var pieParam = {
   "sortOrder": "value-desc",
   "content": [],
   "labels": {
+    formatter: function (context) {
+      var label = context.label;
+      // if it's a single bird seen, add an exclamation mark to the outer label
+      if (context.section === 'inner') {
+          label = label + '%';
+      }
+      if (context.section === 'outer') {
+          label = label + ' - ' + context.value + '滴';
+      }
+      console.log('context=>', context);
+      return label;
+    },
     "outer": {
-      "pieDistance": 32
+      format: "label",
+      "pieDistance": 30
     },
     "inner": {
-      "hideWhenLessThanPercentage": 3
+      format: "percentage",
+      "hideWhenLessThanPercentage": null
     },
     "mainLabel": {
       "fontSize": 11
     },
     "percentage": {
-      "color": "#ffffff",
+      "color": "#fff",
+      "fontSize": 14,
       "decimalPlaces": 0
     },
     "value": {
-      "color": "#adadad",
-      "fontSize": 11
+      "color": "#fff",
+      "fontSize": 14
     },
     "lines": {
-      "enabled": true
+      "enabled": true,
+			style: "curved",
+			color: "segment" // "segment" or a hex color
     },
     "truncation": {
-      "enabled": true
+      "enabled": true,
+      truncateLength: 30
     },
   },
   "effects": {
+    load: {
+      effect: "default", // none / default
+      speed: 500
+    },
     "pullOutSegmentOnClick": {
       "effect": "elastic",
       "speed": 400,
-      "size": 8
-    }
+      "size": 10
+    },
+		highlightSegmentOnMouseover: true,
+		highlightLuminosity: -0.2
   },
   "misc": {
     "gradient": {
-      "enabled": true,
+      "enabled": false,
       "percentage": 100
+    },
+		canvasPadding: {
+			top: 5,
+			right: 5,
+			bottom: 5,
+			left: 5
+		},
+  },
+  tooltips: {
+    enabled: false,
+    type: "placeholder", // caption|placeholder
+    string: "",
+    placeholderParser: null,
+    styles: {
+      fadeInSpeed: 250,
+      backgroundColor: "#000000",
+      backgroundOpacity: 0.5,
+      color: "#efefef",
+      borderRadius: 2,
+      font: "arial",
+      fontSize: 10,
+      padding: 4
     }
   },
+	"header": pieHeader,
+	"footer": pieFooter,
+	"size": pieSize,
+	"data": {
+    content: [],
+  },
+  callbacks: {
+    onload: null,
+    onMouseoverSegment: null,
+    onMouseoutSegment: null,
+    onClickSegment: function(){
+      console.log("[!]click pieChart");
+    },
+	}
 };
 var ajaxSuccess = function (result) {
   for (var i=0; i<result.data.formula.length; i++) {
-    pieDate.content.push({
+    pieParam.data.content.push({
       label: result.data.formula[i].scent,
       value: parseInt(result.data.formula[i].drops),
       color: result.data.formula[i].color,
     });
   }
-	var pie = new d3pie("pieChart", {
-		"header": pieHeader,
-		"footer": pieFooter,
-		"size": pieSize,
-		"data": pieDate,
-	});
+	var pie = new d3pie("pieChart", pieParam);
 }; // end ajaxSuccess
 var ajaxError = function (requestObject, error, errorThrown) {
   sweetAlert("錯誤", "目前無法取得此配方成份！", "error");
