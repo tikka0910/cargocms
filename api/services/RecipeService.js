7c1bd40f130554d39048ea548b7bbd5bd04871c6
FuYaoDe <dan82625@gmail.com>
2016-09-13 15:18:48 +0800

    remove old code

--- api/services/RecipeService.js
+++ api/services/RecipeService.js
@@ -21,14 +21,7 @@ module.exports = {
     authorFbPage
   }) => {
     try {
-
-      // const passport =  await Passport.find({ UserId: recipe.UserId });
-      // const existProvider = typeof passport.provider === 'string';
-      // const checkProviderType = passport.provider === 'facebook';
-      //
-      // if (existProvider && checkProviderType) {
-      //   recipe.authorFbPage = passport.identfier;
-      // }
+      
       recipe.formula = RecipeService.sortFormulaByScentName({ formula: recipe.formula });
       recipe.coverPhotoId = recipe.coverPhotoId == "" ? null : recipe.coverPhotoId;
 
