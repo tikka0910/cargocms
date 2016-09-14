26da66c0dff11f1bf76895dd54efb25cfc55947e
lyhcode <lyhcode@gmail.com>
2016-09-14 11:09:42 +0800

    add facebook config file

--- config/facebook.js
+++ config/facebook.js
@@ -1,4 +1,13 @@
 module.exports.facebook = {
+
+  /*
+   * Find you app token from the following url.
+   * https://developers.facebook.com/tools/accesstoken/
+   */
   accessToken: '',
+
+  /*
+   * http://findmyfbid.com/
+   */
   pageId: '',
 };
