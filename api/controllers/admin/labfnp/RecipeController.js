135cc6e6d33718b11e68f8beaa708acb29b66c6f
KenYA <tikka0910@gmail.com>
2016-09-19 17:17:19 +0800

    修改 recipe controller

--- api/controllers/admin/labfnp/RecipeController.js
+++ api/controllers/admin/labfnp/RecipeController.js
@@ -1,16 +1,27 @@
 module.exports = {
   index: async (req, res) => {
     res.ok({
-      view: true
+      view: true,
+      serverSidePaging: true,
+      layout: 'admin/default/index'
     });
   },
   create: async (req, res) => {
-    res.ok({view: true});
+    res.ok({
+      view: true,
+      layout: 'admin/default/create'
+    });
   },
   edit: async (req, res) => {
-    res.ok({view: true});
+    res.ok({
+      view: true,
+      layout: 'admin/default/edit'
+    })
   },
   show: async (req, res) => {
-    res.ok({view: true});
+    res.ok({
+      view: true,
+      layout: 'admin/labfnp/recipe/show'
+    });
   },
 }
