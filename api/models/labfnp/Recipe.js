28626c9fd60ce25fb3494af44c1fe7d71fa5729b
Kuyen <iamcxa@gmail.com>
2016-09-21 19:46:44 +0800

    add displayFormula field for admin side page recipe index

--- api/models/labfnp/Recipe.js
+++ api/models/labfnp/Recipe.js
@@ -240,6 +240,32 @@ module.exports = {
       }
     },
 
+    displayFormula: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const formulaTotalDrops = this.get('formulaTotalDrops');
+          const formulaJson = JSON.parse(this.getDataValue('formula'));
+          const dpFormulaArray = [];
+          let index = 0;
+
+          for (const formula of formulaJson) {
+            const value = Math.round(( formula.drops / formulaTotalDrops * 100 ) * 10000) / 10000;
+            dpFormulaArray.push({
+              index: index,
+              value: `${formula.scent} - ${formula.drops}滴(${value}%)`
+            });
+            index += 1;
+          }
+          console.log('displayFormula=>', dpFormulaArray);
+
+          return JSON.stringify(dpFormulaArray);
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+
   },
   associations: function() {
     Recipe.hasMany(UserLikeRecipe);
