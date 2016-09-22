140961f819e04d63d1b6aaddfe1644c6b8f1348d
sakuxz <yui12327@gmail.com>
2016-09-22 19:36:52 +0800

    do feedback

--- assets-labfnp/page/recipe/feedback.js
+++ assets-labfnp/page/recipe/feedback.js
@@ -2,30 +2,20 @@ $(document).ready(function () {
 	$('.chosen-select').chosen({
 			allow_single_deselect: false,
 			no_results_text: '按 Enter 新增感覺',
+			placeholder_text_multiple: '   ',
 		});
 
 	$('.chosen-choices .search-field input').keydown(function(event) {
     event.stopPropagation();
 		var feeling = $('.chosen-choices .search-field input').val().trim();
-		if(event.keyCode === 13 && feeling){  //press ENTER
-			var option;
-			var noselect = true;
-      $('.chosen-select option').each(function(i, e){
-				if(i === 0) return;
-				if(!e.selected && noselect) {
-					option = e;
-					noselect = false;
-				}
-			});
+		var IsNoResult = $('.chosen-results').has('.no-results').length > 0;
+		if(event.keyCode === 13 && feeling && IsNoResult){  //press ENTER
 
-			$('.chosen-select').append('<option value="' + feeling + '" selected >' + feeling + '</option>')
-			$('.chosen-select').trigger("chosen:updated");
+			$('.chosen-select').append('<option value="' + feeling + '" selected >' + feeling + '</option>');
 
-		  // setTimeout(function () {
-			// 	console.log(option);
-		  // 	option.selected = false;
-			// 	$('.chosen-select').trigger("chosen:updated");
-		  // }, 0);
+		  setTimeout(function () {
+				$('.chosen-select').trigger("chosen:updated");
+		  }, 200);
 		}
 	});
 });
