909c76fc990169708c3c1b5739abdf28a3c34861
lyhcode <lyhcode@gmail.com>
2016-09-23 17:23:27 +0800

    support assets auto mount

--- config/http.js
+++ config/http.js
@@ -8,9 +8,10 @@
  * For more information on configuration, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.http.html
  */
-var express = require('express');
-var moment = require('moment');
 
+import express from 'express';
+import moment from 'moment';
+import fs from 'fs';
 import linkifyStr from 'linkifyjs/string';
 
 module.exports.http = {
@@ -93,10 +94,15 @@ module.exports.http = {
     // app.use(express.logger());
     // app.use(express.compress());
 
-    app.use('/assets/labfnp', express.static('assets-labfnp'));
-    app.use('/assets/unify', express.static('assets-unify'));
-    app.use('/assets/admin', express.static('assets-admin'));
-    app.use('/assets/library', express.static('assets-library'));
+    fs.readdir('.', function(err, files) {
+      for (var dirName of files) {
+        let isDir = fs.statSync(dirName).isDirectory();
+        if (isDir && dirName.startsWith('assets-')) {
+          sails.log.debug('Setup static assets folder: ' + dirName + ', uri: /' + dirName.replace('-', '/'));
+          app.use('/' + dirName.replace('-', '/'), express.static(dirName));
+        }
+      }
+    });
   },
   middleware: {
     order: [
