c0b987d4a36e121022abcf6c1c6062e09dfaf93f
FuYaoDe <dan82625@gmail.com>
2016-09-23 18:47:34 +0800

    fix recipe id

--- api/services/SocialService.js
+++ api/services/SocialService.js
@@ -10,9 +10,10 @@ module.exports = {
       let socialsConfig = sails.config.socials
 
       const socialData = recipes.map((recipe) => {
-        const {hashId, description} = recipe;
+        const {id, hashId, description} = recipe;
         const title = recipe.perfumeName;
-        const url = SocialService.getShareUrl() + '/recipe/' + hashId
+        const recipeId = hashId || id
+        const url = SocialService.getShareUrl() + '/recipe/' + recipeId
         return {
           description, title, url
         }
