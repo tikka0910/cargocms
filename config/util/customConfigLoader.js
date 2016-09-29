18f6228f013605fa9245a7025fe14161a1dc870b
smlsunxie <smlsun@gmail.com>
2016-09-29 13:11:04 +0800

    close #735

--- config/util/customConfigLoader.js
+++ config/util/customConfigLoader.js
@@ -1,16 +1,22 @@
 import fs from 'fs';
 export default (configName) => {
+  var result = {}
   var files = fs.readdirSync('./config')
   for (var dirName of files) {
     let isDir = fs.statSync('./config/' + dirName).isDirectory();
     if (isDir) {
-      let customRouteConfigFile = './config/' + dirName + '/routes.js';
+      let customRouteConfigFile = __dirname+"/../" + dirName + '/'+configName;
       let hasCustomRouteConfigFile = fs.existsSync(customRouteConfigFile);
       if (hasCustomRouteConfigFile) {
-        return require(__dirname+"/../" + dirName + '/'+configName);
+        var customConfig = require(__dirname+"/../" + dirName + '/'+configName);
+
+        result = {
+          ...result,
+          ...customConfig
+        }
       }
     }
   }
 
-  return {}
+  return result;
 };
